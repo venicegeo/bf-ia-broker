2bb95d3990249d32dd53058fd5ad950acdd7d7f5
Filip Sufitchi <filips@imagemattersllc.com>
2018-06-26 16:46:55 -0400

    Pull explicit bounding box info from DB and expose it in result

--- landsat_localindex/db/model.go
+++ landsat_localindex/db/model.go
@@ -6,10 +6,12 @@ import (
 	"github.com/venicegeo/geojson-go/geojson"
 )
 
+// LandsatLocalIndexScene contains data pertaining to a Landsat8 scene indexed locally
 type LandsatLocalIndexScene struct {
 	ProductID       string
 	AcquisitionDate time.Time
 	CloudCover      float64
 	SceneURLString  string
 	Bounds          *geojson.Polygon
+	BoundingBox     geojson.BoundingBox
 }
