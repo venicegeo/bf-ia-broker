e75d868797dc457d39551b08f022d396a9dc5767
Filip Sufitchi <filips@imagemattersllc.com>
2018-09-04 12:07:08 -0400

    Support multipolygon results for landsat local indes searches on antimeridian

--- landsat_localindex/db/model.go
+++ landsat_localindex/db/model.go
@@ -6,12 +6,20 @@ import (
 	"github.com/venicegeo/geojson-go/geojson"
 )
 
+// SingleOrMultiPolygon is an interface that supports both geojson.Polygon and geojson.MultiPolygon
+type SingleOrMultiPolygon interface {
+	ForceBbox() geojson.BoundingBox
+	Map() map[string]interface{}
+	String() string
+	WKT() string
+}
+
 // LandsatLocalIndexScene contains data pertaining to a Landsat8 scene indexed locally
 type LandsatLocalIndexScene struct {
 	ProductID       string
 	AcquisitionDate time.Time
 	CloudCover      float64
 	SceneURLString  string
-	Bounds          *geojson.Polygon
+	Bounds          SingleOrMultiPolygon
 	BoundingBox     geojson.BoundingBox
 }
